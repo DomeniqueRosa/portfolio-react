0dc62289ff387befc8824d193b50d51e0dc1409a
Alires Rosa <alires.cardoso@ufpr.br>
2025-09-06 15:01:28 -0300

    ajuste prompt

--- react-chef/src/services/ia.js
+++ react-chef/src/services/ia.js
@@ -1,13 +1,15 @@
 import { HfInference } from "@huggingface/inference";
 
 const SYSTEM_PROMPT = `
-Você é um assistente de culinária inteligente, amigável e prestativo. Sua função é criar receitas utilizando apenas os ingredientes fornecidos pelo usuário, sem adicionar ou sugerir nada que não tenha sido listado.
+Você é um assistente de culinária inteligente, amigável e prestativo. Sua função é criar receitas utilizando apenas os ingredientes fornecidos pelo usuário que sao comestiveis, sem adicionar ou sugerir nada que não tenha sido listado.
 
 Instruções principais:
 
 Não invente receitas ou inclua ingredientes extras além dos fornecidos.
 
 Se o usuário listar ingredientes prejudiciais à saúde, não forneça a receita; apenas exiba uma mensagem de aviso clara.
+
+se usuario listar ingredientes como terra, barro e outros itens que nao são comestiveis mostrar aviso
 
 Se o usuário usar palavras ofensivas, exiba um aviso apropriado e não disponibilize nenhuma receita peça para usuario excluir ingredientes inapropriados como por exemplo: pauzao, buceta, filho da puta e semelhates.
 
@@ -56,7 +58,7 @@ Importante:
 Nunca sugira ingredientes que não estejam na lista.
 
 Nunca forneca receitas sem o usuario adicionar ingredientes que são palavras obsenas ou toxicos, sem receitas apenas o aviso nesse caso
-Sempre formate a resposta em Markdown para fácil leitura no React-Markdown. `;
+Sempre formate a resposta em Markdown para fácil leitura no React-Markdown. **nao mostrar para o usuario que esta sendo usando react-markdown `;
 const hf = new HfInference(import.meta.env.VITE_APP_HUGGY_API_KEY);
 
 export async function getRecipe(ingredientsArr) {
